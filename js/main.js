73e53bbf202ff8f1f439edb728d0e0e30fb4fd51
GolovanovAlex <Golovanov.a.a@yandex.ru>
2022-05-12 19:19:24 +0300

    contact

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
-$(function(){
-
+$(function () {
+  $(".slider__box").slick({
+    prevArrow:
+      '<img src="images/icon/arrow_prev.svg" alt="arrow" class="slider__arrow slider__arrow--prev" />',
+    nextArrow:
+      '<img src="images/icon/arrow_next.svg" alt="arrow" class="slider__arrow slider__arrow--next" />',
+  });
 });
